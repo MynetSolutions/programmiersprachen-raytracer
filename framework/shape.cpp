b03bd5eccc277ff941bb97b5542583b717e3c9b5
Arne Osterthun <arne.osterthun@icloud.com>
2015-06-27 16:35:19 +0200

    Added name,color and default constructor to the shape base class

---
+++ framework/shape.cpp
@@ -0,0 +1,4 @@
+#include <shape.hpp>
+
+Shape::Shape(): _name{"untitled_shape"}, _color{0.0} 
+{}
--- framework/shape.hpp
+++ framework/shape.hpp
@@ -1,9 +1,17 @@
 #ifndef BUW_SHAPE_HPP
 #define BUW_SHAPE_HPP
+#include <color.hpp>
+
 class Shape
 {
 	public: 
+		Shape();
+		Shape(std::string name, Color color);
 		virtual double area() const = 0;
 		virtual double volume() const = 0;
+
+	protected:
+		std::string _name;
+		Color _color;
 };
 #endif
